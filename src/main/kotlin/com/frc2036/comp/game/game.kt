9cb1b7dbba432216c2571a7d87b472ad7bbc2ec3
Henry Westfall <henrymwestfall@gmail.com>
2020-08-01 16:34:29 -0600

    add tile flipping (untested)

--- src/main/kotlin/com/frc2036/comp/game/game.kt
+++ src/main/kotlin/com/frc2036/comp/game/game.kt
@@ -1,10 +1,33 @@
 package com.frc2036.comp
 
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 enum class TileType(val type: Int) {
     Empty(0),
     Player1(1),
     Player2(2)
 
+}
+
+enum class Direction(val dx: Int, val dy: Int) {
+    /** cardinal directions **/
+    North(0, -1),
+    South(0, 1),
+    East(-1, 0),
+    West(1, 0),
+
+    /** orthogonal directions **/
+    NorthEast(-1, -1),
+    NorthWest(1, -1),
+    SouthEast(-1, 1),
+    SouthWest(1, -1)
+
+    fun from(point: Pair<Int, Int>): Pair<Int, Int> {
+        val x = point.first + this.dx
+        val y = point.second + this.dy
+        return Pair(x, y)
+
+    }
 }
 
 class Board() {
@@ -37,6 +60,73 @@ class Board() {
         }
     }
 
+    fun inbounds(value: Int): Boolean {
+        return when {
+            value >= 8 -> false
+            value <= 0 -> false
+            else -> true
+        }
+    }
+
+    fun getTileAt(pos: Pair<Int, Int>): TileType {
+        return contents[pos.first][pos.second]
+    }
+
+    fun getFlippedTiles(playerKey: String, x: Int, y: Int): List<Pair<Int, Int>> {
+        val player = when (playerKey) {
+            player1Key -> TileType.Player1
+            player2Key -> TileType.Player2
+            else -> null
+        }
+        val opponent = when (player) {
+            TileType.Player1 -> TileType.Player2
+            TileType.Player2 -> TileType.Player1
+            else -> null
+        }
+
+        val flippedTiles = mutableListOf<Pair<Int, Int>>()
+
+        if (player != null && opponent != null) {
+            // find first friendly stone in each direction
+            for (direction in Direction.values()) {
+                var focus = Pair(x, y)
+                var foundEndpoint = false
+                val path = mutableListOf<Pair<Int, Int>>()
+                do {
+                    focus = direction.from(focus)
+                    if (getTileAt(focus) == player) {
+                        foundEndpoint = true
+                        break
+                    }
+                    else {
+                        path.add(focus)
+                    }
+                } while (inbounds(focus.first) && inbounds(focus.second) && getTileAt(focus) == opponent)
+
+                if (foundEndpoint) flippedTiles.addAll(path)
+            }
+        }
+
+        return flippedTiles.toList()
+    }
+
+    fun checkLegal(key: String, move: Pair<Int, Int>): Boolean {
+        val flippedTiles = getFlippedTiles(key, move.first, move.second)
+        return flippedTiles.isNotEmpty()
+    }
+
+    fun flipTiles(tiles: List<Pair<Int, Int>>) {
+        for (tile in tiles) {
+            val x = tile.first
+            val y = tile.second
+            when (contents[x][y]) {
+                TileType.Player1 -> contents[x][y] = TileType.Player2
+                TileType.Player2 -> contents[x][y] = TileType.Player1
+                TileType.Empty -> println("Warning: tried to flip empty tile.")
+            }
+        }
+    }
+
     fun putMove(player: String, x: Int, y: Int): String? {
         // TODO: ensure incoming moves are legal
         // TODO: resolve move
@@ -48,7 +138,10 @@ class Board() {
 
         var error: String? = null
 
-        if (contents[x][y] == TileType.Empty) contents[x][y] = type // TODO: handle it
+        if (contents[x][y] == TileType.Empty) {
+            flipTiles(getFlippedTiles(player, x, y))
+            contents[x][y] = type
+        }
         else error = "space is occupied"
 
         return error
